89ba9ca46c67bded09c1c9744f08c6725b2e08d5
Matheus <matheusp-2014@outlook.com.br>
2023-09-29 01:00:20 -0300

    outro commit

--- scenario.js
+++ scenario.js
@@ -11,8 +11,6 @@ class Scenario {
         this.render.shadowMap.type = THREE.BasicShadowMap;
 
         this.canvas = this.render.domElement;
-
-        this.curva = undefined;
     }
 
     configLighting() {
