19a310c5b0338e47c21b1e201b070962d1b63f08
Matheus <matheusp-2014@outlook.com.br>
2023-09-29 09:03:18 -0300

    outro commit

--- script.js
+++ script.js
@@ -6,44 +6,46 @@ cam.position.set(0, 0, 1.8);
 var scenario = new Scenario();
 var render = scenario.getRender();
 var canvas = scenario.getCanvas();
-var cena = new THREE.Scene();
+var scene = new THREE.Scene();
 
 document.body.appendChild(canvas);
-cena.background = new THREE.Color( 0xAACCFF );
+
+scene.background = new THREE.Color( 0xAACCFF );
+
+var luzAmbiente = new THREE.AmbientLight(0x333333);
+scene.add(luzAmbiente);
 
 var fundo = scenario.configBackgroundImage('background.png', 4.25, 2.1, 0);
-cena.add(fundo);
+scene.add(fundo);
 
 var curva = scenario.configCurve();
-var path = new THREE.Path(curva.getPoints(300));
 
 for (let p of curva.points) {
     var ponto = scenario.configPoint(p.x, p.y, p.z);
-    cena.add(ponto);
+    scene.add(ponto);
 }
 let lineGeometry = new THREE.BufferGeometry().setFromPoints(curva.getPoints(300));
 let lineMaterial = new THREE.LineBasicMaterial({ color: 0xFFFFFF });
 var linha = new THREE.Line(lineGeometry, lineMaterial);
-cena.add(linha)
+scene.add(linha)
 
-let _ = new THREE.OrbitControls(cam, canvas)
+let controlsCam = new THREE.OrbitControls(cam, canvas)
 
+var path = new THREE.Path(curva.getPoints(300));
 var car = new Car();
 var carro = car.configCar();
-cena.add(carro);
+scene.add(carro);
 
 var posicao = 0;
 var angulo = 0;
-
 var light = scenario.configLighting();
-light.position.set(0.5, 0, 5);
-cena.add(light);
-
+light.position.set(0.6, 0, 6);
+scene.add(light);
 
 function desenhar() {
     car.movement();
     
-    render.render(cena, cam);
+    render.render(scene, cam);
     
     requestAnimationFrame(desenhar);
 }
